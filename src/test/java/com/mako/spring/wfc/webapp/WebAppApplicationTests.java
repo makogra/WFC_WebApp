f26683cfa26a26d77b40c0c7c513d9a2cc9b518c
makogra <pietrzak.maciek.pv@gmail.com>
2023-11-05 07:16:35 +0100

    innit Project

---
+++ src/test/java/com/mako/spring/wfc/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mako.spring.wfc.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
